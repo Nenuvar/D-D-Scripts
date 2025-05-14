f8cc737cbc8e91765ce30e5839f25ee013c67b05
nenuvar <oliveroverenghalvorsen@gmail.com>
2025-05-14 09:11:49 +0200

    Update encounter_generator.py

--- dnd-encounter-generator/src/encounter_generator.py
+++ dnd-encounter-generator/src/encounter_generator.py
@@ -1,1 +1,11 @@
+# filepath: src/encounter_generator.py
+import json  # This is a built-in Python library for working with JSON files.
 
+def load_monsters(file_path):
+    """
+    Loads monster data from a JSON file.
+    :param file_path: Path to the JSON file.
+    :return: A list of monsters.
+    """
+    with open(file_path, 'r') as file:  # Open the file in read mode.
+        return json.load(file)  # Parse the JSON data and return it.
